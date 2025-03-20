263fce58b752ff5db753f7038c66dc1af7636d58
Akke <sirpurpleness@gmail.com>
2025-03-20 11:22:21 +0100

    separated into subcomponents

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -4,7 +4,7 @@ import ViewAuction from "../components/ViewAuction/ViewAuction";
 const Router = () => {
     return (
         <Routes>
-            <Route exact path="/view" element={<ViewAuction />}></Route>
+            <Route exact path="/auction/:id" element={<ViewAuction />}></Route>
         </Routes>
     )
 }
