efc2c20b3f27af78487a8dab55a0eaf8a8a88f4a
andreasLoetzsch <andreas.lotzsch@gmail.com>
2025-03-25 13:53:11 +0100

    feat: added getAllAuctions and getAuction

--- backend/src/routes/auctionRoutes.js
+++ backend/src/routes/auctionRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {createAuction, deleteAuction, updateAuction, placeBid, updateBid} = require('../controllers/auctionController')
+const {createAuction, deleteAuction, updateAuction, placeBid, updateBid, getAuction, getAllAuctions} = require('../controllers/auctionController')
 const {verifyAuth} = require('../middleware/authMiddleware')
 
 router.delete('/:id', [verifyAuth], deleteAuction)
 router.post('/', [verifyAuth], createAuction)
 router.post('/placeBid', [verifyAuth], placeBid)
+router.get('/', getAllAuctions)
+router.get('/:id', getAuction)
 router.patch('/updateBid', [verifyAuth], updateBid)
 router.patch('/:id', [verifyAuth], updateAuction )
 
