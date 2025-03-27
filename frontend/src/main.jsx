98efd0f1c0fd9c14f98d2373c88a31a7bb01c67b
Akke <sirpurpleness@gmail.com>
2025-03-27 09:42:16 +0100

    account login stuff

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuctionProvider from './context/AuctionProvider.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuctionProvider>
-        <App />
-    </AuctionProvider>
+    <AuthProvider>
+      <AuctionProvider>
+          <App />
+      </AuctionProvider>
+    </AuthProvider>
   </StrictMode>,
 )
--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -4,15 +4,30 @@ import Layout from "../components/Layout";
 import AuctionsList from "../Components/AuctionsList/AuctionsList";
 import AccountRegister from "../components/AccountRegister/AccountRegister";
 import AccountLogin from "../components/AccountLogin/AccountLogin";
+import ProtectedRoute from "./ProtectedRoute";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import PublicRoute from "./PublicRoute";
 
 const Router = () => {
+    const { user } = useContext(AuthContext);
+
+    /*
+    * Vill du skapa en "protected" route där man måste vara inloggad?
+        <Route exact path="/view/:id" element={<ProtectedRoute isAuthenticated={user !== null} />}>
+            <Route index element={<Layout><ViewAuction /></Layout>} />
+        </Route>
+    */
+
     return (
         <>
             <Routes>
                 <Route exact path="/" element={<Layout><AuctionsList/></Layout>}></Route>
                 <Route exact path="/register" element={<Layout><AccountRegister/></Layout>}></Route>
-                <Route exact path="/login" element={<Layout><AccountLogin/></Layout>}></Route>
-                <Route exact path="/view/:id" element={<Layout><ViewAuction /></Layout>}></Route>
+                <Route exact path="/login" element={<PublicRoute isAuthenticated={user !== null} />}>
+                    <Route index element={<Layout><AccountLogin /></Layout>} />
+                </Route>
+                <Route exact path="/view/:id" element={<Layout><ViewAuction /></Layout>} />
             </Routes>
         </>
     )
